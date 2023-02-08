230679b501640e942a9ce08eb39ff51489472ca2
YooZiiX <jeremy.lucas@mines-ales.org>
2023-02-08 18:30:19 +0100

    1st Commit : Web Scraping

--- main.py
+++ main.py
@@ -2,22 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-cities = []
+villes = []
 dates = []
+ensoleillements = []
+temperatures = []
+temperatures_resentie = []
+precipitations = []
+probabilites = []
+direction_vent = []
+vitesse = []
+vitesse_rafale = []
+humidites = []
+nebulosites = []
 
 def getUrl(codepostal):
     url = "https://meteofrance.com/previsions-meteo-france/" + str(codepostal)
     return url
 
 #def extract_info(city, codepostal):
-page = requests.get('https://www.lameteoagricole.net/meteo-heure-par-heure/Rouen-35000-j1.html')
+page = requests.get('https://www.lameteoagricole.net/meteo-heure-par-heure/Al%C3%A8s-30100-j1.html')
 soup = BeautifulSoup(page.text, 'html.parser')
 body_text = soup.find("div", attrs={'class': 'col-lg-8 col-xxl-9'})
 
 #Obtenir la ville:
 header = body_text.find("div", attrs={'class': 'table-outter-header'})
-city = header.find("span", attrs={'class': 'fs-5 p-5'})
-cities.append(city.text)
+ville = header.find("span", attrs={'class': 'fs-5 p-5'})
 
 #Obtenir le tableau d'informations:
 indicTable = body_text.find("table", attrs={'id': 'heures-table'})
@@ -34,7 +43,44 @@ for date in Dates:
 indicRowBody = indicTable.find('tbody')
 indicRow = indicRowBody.findAll('td')
 for indic in indicRow:
+    villes.append(ville.text)
     indicDiv = indic.findAll('div', attrs={'class': 'd-flex flex-column mb-3 showDetailsBtn'})
-    #print(indicDiv)
-    for value in indicDiv:
-        print(value)
+    #Ensoleillement:
+    sunshine = indicDiv[0].find('span', attrs = 'forModal')
+    ensoleillements.append(sunshine.text)
+    #Températures:
+    temp = indicDiv[1].find('span', attrs = 'fw-bold fs-4 text-warning')
+    temperatures.append(temp.text[:-1]) # [:-1] pour retirer le °
+    tempR = indicDiv[1].find('span', attrs = 'fs-5 text-secondary noModal')
+    temperatures_resentie.append(tempR.text[:-1]) # [:-1] pour retirer le °
+    #Précipitations & probabilités:
+    prec = indicDiv[2].find('span', attrs = 'fw-bold')
+    precipitations.append(prec.text)
+    prob = indicDiv[2].find('span', attrs = 'small text-shade-3 noModal')
+    probabilites.append(prob.text[:-1]) # [:-1] pour retirer le %
+    #Vents (Direction, Vitesse, Vitesse Rafale):
+    dir = indicDiv[3].find('span', attrs = 'small')
+    direction_vent.append(dir.text)
+    vitesseVent = indicDiv[3].find('span', attrs = 'fw-bold')
+    vitesse.append(vitesseVent.text)
+    vitessesVent = indicDiv[3].findAll('span')
+    vitesse_rafale.append(vitessesVent[7].text[:-5])
+    #Humidités:
+    humidite = indicDiv[4].find('span', attrs = 'fw-bold')
+    humidites.append(humidite.text)
+    #Nébulosités:
+    nebulosite = indicDiv[5].find('span', attrs = 'fw-bold')
+    nebulosites.append(nebulosite.text)
+
+
+print(villes)
+print(dates)
+print(ensoleillements)
+print(temperatures)
+print(temperatures_resentie)
+print(precipitations)
+print(probabilites)
+print(direction_vent)
+print(vitesse)
+print(vitesse_rafale)
+print(humidites)
